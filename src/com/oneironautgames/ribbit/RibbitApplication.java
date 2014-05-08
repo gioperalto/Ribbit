8905c398dde4389c1ced17bfd85554ef1eb9409c
gioperalto <gyoperalto@gmail.com>
2014-05-08 18:06:45 -0400

    added parse back end; broke inbox and friends into fragments; implemented friends list

---
+++ src/com/oneironautgames/ribbit/RibbitApplication.java
@@ -0,0 +1,13 @@
+package com.oneironautgames.ribbit;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class RibbitApplication extends Application {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		Parse.initialize(this, "YFLfZ1hzfZ9ELmNjObxnRgwbmtHOvEe5KbHTlq5i", "4Kypk3Rm7bcvDQ6PiMV6wqMt1Uojxr857yNBQSrQ");
+	}
+}
