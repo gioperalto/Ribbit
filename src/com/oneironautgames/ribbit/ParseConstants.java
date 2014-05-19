8151230983bd04364b72c3f311c97ef726872e42
gioperalto <gyoperalto@gmail.com>
2014-05-19 11:35:57 -0400

    finished basic project; lots of updates... fml

--- src/com/oneironautgames/ribbit/ParseConstants.java
+++
@@ -1,9 +0,0 @@
-package com.oneironautgames.ribbit;
-
-public final class ParseConstants {
-	// Class names
-	
-	// Field names
-	public static final String KEY_USERNAME = "username";
-	public static final String KEY_FRIENDS_RELATION = "friendsRelation";
-}
